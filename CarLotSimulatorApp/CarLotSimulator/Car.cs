872448caa0edc8fae5eec4db36b6326683402615
Rhea Epperson <re.epperson2@gmail.com>
2023-02-09 13:26:41 -0500

    exercise 2 complete

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+	}
+}
+
---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class CarLot
+	{
+		public CarLot()
+		{
+		}
+	}
+}
+
